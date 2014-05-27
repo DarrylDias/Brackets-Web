3937f53f712e58f1f69c44544c235900126ae99d
darryldias <darryldias@live.com>
2014-05-27 21:46:49 +0530

    Adding platform dectection function

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ res.end('Hello World');
 
 console.log("\n Starting... \n");
 console.log("\n Visit http://127.0.0.1:3000/bracket \n");
+console.log("\n Platform detected: " + process.platform);
 console.log("\n Press CTRL + C to stop \n ");
