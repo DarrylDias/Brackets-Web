0fe4526eb164189db7f1fcdd6147bbee2bc7c669
darryldias <darryldias@live.com>
2014-05-27 23:29:28 +0530

    Adding Platform detection and better console output.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var connect = require('connect'),
 brackets    = require('brackets');
 
+var host = "127.0.0.1";
+var port = 3000;
+
 connect()
 .use('/brackets', brackets())
 .use(function (request, response) {
 res.end('Hello World');
-}).listen(3000);
+}).listen(port, host);
 
 console.log("\n Starting... \n");
 console.log("\n Platform detected: " + process.platform);
-console.log("\n Visit http://127.0.0.1:3000/bracket \n");
+console.log("\n" + "Server running at:" + host + port + "/brackets" + "\n");
 console.log("\n Press CTRL + C to stop \n ");
