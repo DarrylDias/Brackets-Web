4f6255571a8d91e77421c575caf1b35bf1496d17
darryldias <darryldias@live.com>
2014-05-27 23:20:11 +0530

    Replacing console output arrangements

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ res.end('Hello World');
 }).listen(3000);
 
 console.log("\n Starting... \n");
-console.log("\n Visit http://127.0.0.1:3000/bracket \n");
 console.log("\n Platform detected: " + process.platform);
+console.log("\n Visit http://127.0.0.1:3000/bracket \n");
 console.log("\n Press CTRL + C to stop \n ");
